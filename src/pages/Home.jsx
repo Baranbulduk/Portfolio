7703d69d680ba89778e7e54b8a1b7d1ba8b24ce5
Baranbulduk <baranbulduk99@gmail.com>
2024-05-02 18:09:05 +0200

    API & Router tillagd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import FetchData from '../components/Fetchdata/FetchData';
 
 function Home(){
     return(
     <>
     <h1>Home</h1>
+    <FetchData />
     <Link to="/about">
         <button>About</button>
       </Link>
